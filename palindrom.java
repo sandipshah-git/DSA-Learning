22c37ab1791b5e6861a5d10d8633e8781b41a4a8
sandipsah-git <sandip123456sah@gmail.com>
2025-10-15 14:38:47 +0545

    commit

---
+++ palindrom.java
@@ -0,0 +1,47 @@
+//  1_Palindrome Check if a given string is a palindrome (ignore spaces and case).
+//  Example: "Race car" → True
+
+
+/*
+        	\\s → matches spaces, tabs, new lines, any whitespace
+			\s  →  this allow for only space
+			"" → replaces those whitespaces with nothing (deletes them)
+			               Why double backslash \\?
+			In Java strings, a single backslash \ is considered an escape character.
+			So to write a real backslash, you must type \\
+			Therefore, "\\s" becomes \s in regex(regular expression), which means space/whitespace
+			
+			Escape Characters are special characters in programming used 
+			to represent non-printable or special-purpose symbols inside a string (\n  \t  \r   \s).
+			*/
+
+public class palindrom {
+	public static void main(String[] args) {
+		String str = "Ra\tce  \ncar";
+
+		// Remove whitespace and convert to lowercase
+		str = str.replaceAll("\\s","").toLowerCase();  // str.replaceAll(" ","");  // this remove only space (\s is also do same)
+
+		System.out.println("Entered String (after cleaning) : " + str);
+
+		int left = 0;
+		int right = str.length()-1;
+		boolean isPalindrome = true;
+
+		while (left < right) {
+			if(str.charAt(left) != str.charAt(right)){
+				isPalindrome = false;
+				break; //  Stops loop immediately (saves time)
+			}
+			left++;
+			right--;
+		}
+
+		if(isPalindrome){
+			System.out.println("It's palindrom.");
+		}
+		else{
+			System.out.println("It's not palindrom.");
+		}
+	}
+}
